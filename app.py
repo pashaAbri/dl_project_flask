1cf82bcb5f49e7bc368853443f6a2f8a32d43557
pashaAbri <39867050+pashaAbri@users.noreply.github.com>
2023-11-29 14:51:22 -0500

    adding cors for our sample aws react endpoint

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
+
 from routes import bp as bp_routes
 
 app = Flask(__name__)
+CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "https://tlkrizdks0.execute-api.us-east-1.amazonaws.com"}})
 
 # register the routes blueprint
 app.register_blueprint(bp_routes)
