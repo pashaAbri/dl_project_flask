ce18f6330adb0274d557cccdf818d540e6b4c7b7
pashaAbri <39867050+pashaAbri@users.noreply.github.com>
2023-12-14 00:43:44 -0500

    cleanup

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flask_cors import CORS
 from routes import bp as bp_routes
 
 app = Flask(__name__)
+
+# List of allowed origins
+allowed_origins = [
+    "https://tlkrizdks0.execute-api.us-east-1.amazonaws.com",
+]
+
 CORS(app)
-CORS(app, resources={r"/api/*": {"origins": "https://tlkrizdks0.execute-api.us-east-1.amazonaws.com"}})
+CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 
 # register the routes blueprint
 app.register_blueprint(bp_routes)
